1ce8af8bbcc8032c4d34b619f355a1e05be61b51
Shubhankar <shubhankar.andi5@gmail.com>
2021-07-01 22:39:22 +0530

    Added routes and homeController

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 
 const port = 8056;
 
+// use express router
+app.use('/', require('./routes'));
+
 app.listen(port, (err) => {
     if (err) console.log(`Error starting the server ${err}`);
     console.log(`Server is running on localhost:${port}`);
