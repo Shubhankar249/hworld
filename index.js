be43985d792426ef51541343d674cf3f213ee20c
Shubhankar <shubhankar.andi5@gmail.com>
2021-07-01 23:05:00 +0530

    added ejs

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8056;
 // use express router
 app.use('/', require('./routes'));
 
+// set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, (err) => {
     if (err) console.log(`Error starting the server ${err}`);
     console.log(`Server is running on localhost:${port}`);
--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/posts_controller');
 
-router.get('/profile', postsController.posts);
+router.get('/', postsController.posts);
 
 module.exports = router;
